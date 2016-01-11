9015458c97a06fd5ac061487392f319f302e9020
263108283@qq.com <263108283@qq.com>
2016-01-11 13:50:03 +0800

    commit log

---
+++ WondersGroup.BioBank/WondersGroup.BioBank.Log/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WondersGroup.BioBank.Log
+{
+    public class Class1
+    {
+    }
+}
